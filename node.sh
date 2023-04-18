9e9b7a8cda1860a1dc516d98485e59a3b7dbacea
UDAY PRATAP SINGH <31985102+udayfop@users.noreply.github.com>
2023-04-19 01:16:10 +0530

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Change to the directory of your Node.js application
+cd /path/to/your/node/app
+
+# Install dependencies
+npm install
+
+# Build the application
+npm run build
+
+# Start the application with pm2
+pm2 start admin --name "admin"
+
+# Save the pm2 process list so it will automatically restart on reboot
+pm2 save
